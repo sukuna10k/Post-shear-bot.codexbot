13944da84dee41a30ff936a797fa517523389e44
Chiranjib Koch <79559271+ChiranjibKoch@users.noreply.github.com>
2024-07-14 12:07:39 +0530

    Implement env support for configuration values

--- info.py
+++ info.py
@@ -1,8 +1,14 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  =
-ADMIN        = 1255023013
-CHANNEL      = "RahulReviews"
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+SESSION = os.getenv("SESSION", "")
+DATABASE_URI = os.getenv("DATABASE_URI", "")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "0"))
+ADMIN = int(os.getenv("ADMIN", ""))
+CHANNEL = os.getenv("CHANNEL", "")
